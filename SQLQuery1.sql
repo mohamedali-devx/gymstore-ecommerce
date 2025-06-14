381c96fb8dcae87422c64732cd2dbe5976e2254c
Mohamed Ali <mohamedaliabdelgalil12@gmail.com>
2025-06-14 21:40:02 +0300

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,70 @@
+CREATE DATABASE ecommerce;
+-- Users Table
+CREATE TABLE users (
+    user_id INT IDENTITY(1,1) PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    address TEXT NOT NULL,
+    role VARCHAR(20) CHECK (role IN ('Customer', 'Admin')) NOT NULL DEFAULT 'Customer'
+);
+
+-- Categories Table
+CREATE TABLE categories (
+    category_id INT IDENTITY(1,1) PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT
+);
+
+-- Products Table
+CREATE TABLE products (
+    product_id INT IDENTITY(1,1) PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    price DECIMAL(10,2) NOT NULL,
+    stock_quantity INT NOT NULL,
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES categories(category_id) ON DELETE SET NULL
+);
+
+-- Orders Table
+CREATE TABLE orders (
+    order_id INT IDENTITY(1,1) PRIMARY KEY,
+    user_id INT,
+    order_date DATETIME DEFAULT GETDATE(),
+    status VARCHAR(20) CHECK (status IN ('Pending', 'Shipped', 'Delivered', 'Cancelled')) DEFAULT 'Pending',
+    total_price DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+-- Order Items Table --> Junction table between orders and products (M:M)
+CREATE TABLE order_items (
+    order_id INT,
+    product_id INT,
+    quantity INT NOT NULL,
+    subtotal_price DECIMAL(10,2) NOT NULL,
+    PRIMARY KEY (order_id, product_id),  -- Composite Primary Key
+    FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES products(product_id) ON DELETE CASCADE
+);
+
+-- Shopping Cart Table
+CREATE TABLE shopping_cart (
+    cart_id INT IDENTITY(1,1) PRIMARY KEY,
+    user_id INT,
+    product_id INT,
+    quantity INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES products(product_id) ON DELETE CASCADE
+);
+
+-- Payments Table
+CREATE TABLE payments (
+    order_id INT PRIMARY KEY,  -- Making order_id the primary key (1:1 relation)
+    payment_method VARCHAR(20) CHECK (payment_method IN ('Credit Card', 'PayPal', 'Bank Transfer')) NOT NULL,
+    payment_status VARCHAR(20) CHECK (payment_status IN ('Pending', 'Completed', 'Failed')) DEFAULT 'Pending',
+    transaction_id VARCHAR(100) UNIQUE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE CASCADE
+);
+DROP TABLE order_items;
